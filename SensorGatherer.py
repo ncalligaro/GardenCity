dd87a848bcd711bf9f3d367277be2db4b475d5c1
ncalligaro <nicolascalligaro@gmail.com>
2018-01-20 15:57:00 +0000

    Added database insertion capabilities

--- SensorGatherer.py
+++ SensorGatherer.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
 import serial
 import datetime
 import time
 import re
+import traceback
+
+import mysql.connector as mariadb
 
 import sys
 import RPi.GPIO as GPIO
@@ -19,6 +23,16 @@ radio = NRF24(GPIO, spidev.SpiDev())
 
 SCRIPT_START_DATE = datetime.datetime.utcnow()
 MEASUREMENTS_FOLDER = 'measurements'
+
+SAVE_TO_FILE = True
+SAVE_TO_DB = True
+
+def connect_to_db():
+    try:
+        return mariadb.connect(user='XX', password='XX', host='XX', database='XX')
+    except mariadb.Error as error:
+        print("Error opening connection to DB: {}".format(error))
+        raise
 
 def configure_radio():
     pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
@@ -80,17 +94,36 @@ def get_remote_sensor_data():
     radio.stopListening()
     return RH, T
 
-def save_record(place, humidity, temperature, measureTime, creationTime):
-    f = open('%s/temps_%s.txt' % (MEASUREMENTS_FOLDER, SCRIPT_START_DATE.strftime("%Y-%m-%d_%H%M")),'a')
+def save_record(place, value, valueType, measureTime, creationTime):
 
-    if humidity is not None and temperature is not None:
-        f.write("INSERT INTO measurement (place,type,value,unit,measurement_date,created_at) VALUES ('%s','humidity',%s,'P','%s','%s');\n" % (place, humidity, measureTime, creationTime))
-        f.write("INSERT INTO measurement (place,type,value,unit,measurement_date,created_at) VALUES ('%s','temperature',%s,'C','%s','%s');\n" % (place, temperature, measureTime, creationTime))
+    sqlSentence = None
+    if value is not None and valueType is not None:
+        sqlSentence = ("INSERT INTO measurement (place,type,value,unit,measurement_date,created_at) VALUES ('%s','%s',%s,'P','%s','%s');\n" % (place, valueType, value, measureTime, creationTime))
     else:
-        f.write("INSERT INTO measurement (place,type,value,unit,measurement_date,created_at) VALUES ('%s','humidity',%s,'P','%s','%s');\n" % (place, 'null', measureTime, creationTime))
-        f.write("INSERT INTO measurement (place,type,value,unit,measurement_date,created_at) VALUES ('%s','temperature',%s,'C','%s','%s');\n" % (place, 'null', measureTime, creationTime))
+        sqlSentence = ("INSERT INTO measurement (place,type,value,unit,measurement_date,created_at) VALUES ('%s','%s',%s,'P','%s','%s');\n" % (place, valueType, 'null', measureTime, creationTime))
     
-    f.close()
+    if (SAVE_TO_FILE):
+        try:
+            f = open('%s/temps_%s.txt' % (MEASUREMENTS_FOLDER, SCRIPT_START_DATE.strftime("%Y-%m-%d_%H%M")),'a')
+            #SAVE
+            f.write(sqlSentence)
+            f.close()
+        except Error as error:
+            print("Error saving to file: {}".format(error))
+
+    if (SAVE_TO_DB):
+        db_connection = None
+        try:
+            db_connection = connect_to_db()
+            cursor = db_connection.cursor()
+            cursor.execute(sqlSentence)
+            db_connection.commit()
+        except mariadb.Error as error:
+            print("Error saving to DB: {}".format(error))
+        finally:
+            if db_connection is not None:
+                db_connection.close()
+
 
 def main():
     try:
@@ -99,10 +132,12 @@ def main():
             now = datetime.datetime.utcnow()
 
             DRH, DT = get_local_sensor_data()
-            save_record('Dining', DRH, DT, now.isoformat(), now.isoformat())
+            save_record('Dining', DRH, 'humidity', now.isoformat(), now.isoformat())
+            save_record('Dining', DT, 'temperature', now.isoformat(), now.isoformat())
 
             RRH, RT = get_remote_sensor_data()
-            save_record('Room', RRH, RT, now.isoformat(), now.isoformat())
+            save_record('Room', RRH, 'humidity', now.isoformat(), now.isoformat())
+            save_record('Room', RT, 'temperature', now.isoformat(), now.isoformat())
 
             time.sleep(60)
     except KeyboardInterrupt:
@@ -110,6 +145,7 @@ def main():
     except Exception as e:
         print "\nOther error occurred"
         print (e)
+        print(traceback.format_exc())
     finally:
         print "\nCleaning GPIO port\n"
         GPIO.cleanup()
