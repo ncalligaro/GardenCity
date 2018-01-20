3d038902049d09a2cdaded9c1ad589ce72e960d0
ncalligaro <nicolascalligaro@gmail.com>
2018-01-20 12:06:08 +0000

    v2 db schema

--- config/schema.sql
+++ config/schema.sql
@@ -1,28 +1,16 @@
 CREATE TABLE
-    humidity
+    measurement
     (
         id bigint unsigned NOT NULL AUTO_INCREMENT,
-        room VARCHAR(50),
-        humidity DOUBLE,
+        place VARCHAR(50),
+        value DOUBLE,
         unit VARCHAR(25),
         measurement_date TIMESTAMP NULL,
         created_at TIMESTAMP NULL,
-        updated_at TIMESTAMP NULL,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON
+    UPDATE
+        CURRENT_TIMESTAMP,
+        type VARCHAR(50),
         PRIMARY KEY (id)
     )
     ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE
-    temperature
-    (
-        id bigint unsigned NOT NULL AUTO_INCREMENT,
-        room VARCHAR(50),
-        temperature DOUBLE,
-        unit VARCHAR(25),
-        measurement_date TIMESTAMP NULL,
-        created_at TIMESTAMP NULL,
-        updated_at TIMESTAMP NULL,
-        PRIMARY KEY (id)
-    )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
-    
