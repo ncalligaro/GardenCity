4578ee51a650bfb334ebb3213877c64164375b29
ncalligaro <nicolascalligaro@gmail.com>
2018-01-28 17:53:51 +0000

    added config for multi-script setup

--- app/config/config.sample.py
+++ app/config/config.sample.py
@@ -11,10 +11,17 @@ file = { 'save_to_file': True,
 		 'suffix' : '%Y-%m-%d',
 		 'path' : 'someFolderWithinCurrentPWD' }
 
+
+
 open_map = { 'host': 'api.openweathermap.org',
              'path': '/data/2.5/weather',
              'api_key': '',
 			 'city': 'London,uk' }
 
+local_sensor = { 'gpio_port': 23,
+                 'location_name': "Dining" }
+
+remote_arduino_sensor = { 'gpio_port': 17,
+                          'location_name': "Room" }
 
 
