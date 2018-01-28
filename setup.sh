2c91a40234811a68e35c955df3d719e7d7f3973c
ncalligaro <nicolascalligaro@gmail.com>
2018-01-28 18:42:17 +0000

    added config commands, not yet a proper script though

--- setup.sh
+++ setup.sh
@@ -13,4 +13,37 @@ sudo python setup.py install
 cd ..
 rm -r Adafruit_Python_DHT
 
+#mkdir -p ~/.config/systemd/user/
+#cp scripts/gardenCity.service ~/.config/systemd/user/
+#chmod +x ~/.config/systemd/user/gardenCity.service
+#cp scripts/gardenCity.service ~/.config/systemd/user/
+#chmod +x ~/.config/systemd/user/gardenCity.service
+
+
+mv scripts/gardenCity_WebSensor.service.app scripts/gardenCity_WebSensor.service
+mv scripts/gardenCity_RemoteSensor.service.app scripts/gardenCity_RemoteSensor.service
+mv scripts/gardenCity_LocalSensor.service.app scripts/gardenCity_LocalSensor.service
+mv scripts/gardenCity_HeaterControl.service.app scripts/gardenCity_HeaterControl.service
+sudo cp /home/pi/GardenCity/scripts/* /etc/systemd/system/ 
+
+sudo systemctl daemon-reload
+sudo systemctl enable gardenCity_WebSensor
+sudo systemctl start gardenCity_WebSensor
+sudo systemctl status gardenCity_WebSensor.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable gardenCity_RemoteSensor
+sudo systemctl start gardenCity_RemoteSensor
+sudo systemctl status gardenCity_RemoteSensor.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable gardenCity_LocalSensor
+sudo systemctl start gardenCity_LocalSensor
+sudo systemctl status gardenCity_LocalSensor.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable gardenCity_HeaterControl
+sudo systemctl start gardenCity_HeaterControl
+sudo systemctl status gardenCity_HeaterControl.service
+
 echo 'Install NR24 bla'
