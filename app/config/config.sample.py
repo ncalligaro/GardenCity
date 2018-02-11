3b5b4a88838a86f31efd86cfe4d61be4c004b360
ncalligaro <nicolascalligaro@gmail.com>
2018-02-11 11:34:12 +0000

    normalized config to cater for future attrs and added debug variable

--- app/config/config.sample.py
+++ app/config/config.sample.py
@@ -4,24 +4,31 @@ mysql = { 'host': '',
           'user': '',
           'password':'',
           'database':'',
-          'save_to_DB': True }
+          'save_to_DB': True,
+           }
 
 file = { 'save_to_file': True,
 		 #'suffix' : '%Y-%m-%d_%H%M',
 		 'suffix' : '%Y-%m-%d',
-		 'path' : 'someFolderWithinCurrentPWD' }
+		 'path' : 'someFolderWithinCurrentPWD',
+      }
 
 
 
 open_map = { 'host': 'api.openweathermap.org',
              'path': '/data/2.5/weather',
              'api_key': '',
-			 'city': 'London,uk' }
+			       'city': 'London,uk',
+              }
 
 local_sensor = { 'gpio_port': 23,
-                 'location_name': "Dining" }
+                 'location_name': "Dining",
+                  }
 
 remote_arduino_sensor = { 'gpio_port': 17,
-                          'location_name': "Room" }
+                          'location_name': "Room",
+                           }
 
+runtime_variables = { 'debug' : False,
+                      }                    
 
