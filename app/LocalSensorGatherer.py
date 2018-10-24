b4a1a28c62df53bbd00491244d7e91f60675282c
ncalligaro <nicolascalligaro@gmail.com>
2018-10-24 22:22:48 +0100

    Added avg check to avoid getting spikes in sensor data when sensor has erroneous readings

--- app/LocalSensorGatherer.py
+++ app/LocalSensorGatherer.py
@@ -26,6 +26,12 @@ logging.basicConfig(level=config.get_logging_level(),
 
 GPIO.setmode(GPIO.BCM)
 
+recent_values = {}
+recent_values['humidity'] = []
+recent_values['temperature'] = []
+number_of_values_to_average = 5
+#max_variation_in_units = 9
+
 def get_local_sensor_data(sensor_config):
     sensor_model = sensor_config['sensor_model']
     RH, T = None, None
@@ -48,6 +54,31 @@ def get_local_sensor_data(sensor_config):
 
     return None, None
 
+def is_within_range(type, value, sensor_config):
+    global recent_values
+    if len(recent_values[type]) < number_of_values_to_average:
+        logging.info("There are less than %s values for %s. Adding %s to list" % (number_of_values_to_average, type, value))
+        return True
+
+    logging.info("Average of array for type %s is %s" % (type, sum(recent_values[type])/len(recent_values[type])))
+    max_value = sum(recent_values[type])/len(recent_values[type]) + sensor_config['variation_margin_units']
+    min_value = sum(recent_values[type])/len(recent_values[type]) - sensor_config['variation_margin_units']
+    if min_value < value < max_value:
+        logging.debug("There are more than %s values for %s. Value %s was within range" % (number_of_values_to_average, type, value))
+        return True
+
+    logging.info("Measure of %s %s was not within range. Ignoring value" % (type, value))
+    return False
+
+def update_recent_list(type, value):
+    global recent_values
+    if len(recent_values[type]) < number_of_values_to_average:
+        recent_values[type].append(value)
+    else:
+        recent_values[type] = recent_values[type][1:]
+        recent_values[type].append(value)
+    logging.info("New len of array for type %s is %s" % (type, len(recent_values[type])))
+
 def main():
     logging.info("Saving to file: %s" % (config.file['save_to_file']))
     logging.info("Saving to DB: %s" % (config.mysql['save_to_DB']))
@@ -58,10 +89,13 @@ def main():
                 now = datetime.datetime.utcnow()
 
                 LRH, LT = get_local_sensor_data(sensor)
-                if LRH is not None:
+
+                if LRH is not None and is_within_range('humidity', float(LRH), sensor):
                     commonFunctions.save_humidity_data(sensor['location_name'], LRH, now.isoformat(), now.isoformat())
-                if LT is not None:
+                    update_recent_list('humidity', float(LRH))
+                if LT is not None and is_within_range('temperature', float(LT), sensor):
                     commonFunctions.save_temperature_data(sensor['location_name'], LT, now.isoformat(), now.isoformat())
+                    update_recent_list('temperature', float(LT))
 
             sleep(config.local_sensors['sleep_time_in_seconds_between_reads'])
     except KeyboardInterrupt:
